7911ff3a37214447526591fc1db2285f248b2d1d
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2025-02-09 23:31:08 -0500

    Planned out the api interface

--- url-store/api/apimanager.h
+++ url-store/api/apimanager.h
@@ -2,14 +2,39 @@
 #define APIMANAGER_H
 
 #include <QObject>
+#include <QNetworkAccessManager>
 
+// Abstract class for the LLM API
 class ApiManager : public QObject
 {
     Q_OBJECT
+
+protected:
+    enum RequestType {
+        FetchUrlPage,
+        FetchTags,
+    };
+
+    QNetworkAccessManager *networkManager;
+    QHash<QNetworkReply *, RequestType> requestTypeMap;
+    QString apiKey;
+
 public:
     explicit ApiManager(QObject *parent = nullptr);
 
+    virtual void setApiKey(const QString &apiKey) = 0;
+
+    virtual void fetchTags(const QString &url) = 0;
+    virtual void fetchPageContent(const QString &url) = 0;
+
+    virtual void cancelRequest() = 0;
+
 signals:
+    void tagsFetched(const QStringList &tags);
+    void pageFetched(const QString &content);
+    void errorFetchingData(const QString &error);
+
+
 };
 
 #endif // APIMANAGER_H
